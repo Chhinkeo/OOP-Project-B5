3b64b941acb0d7e8c4e017ff42542a345af3d6aa
PHAL.HIM <PHAL.HIM@PNC.LAN>
2024-04-28 13:33:30 +0700

    [feat] create pilot, attantent, and airleneChef #15

---
+++ src/Person/employee/AirlineChef.ts
@@ -0,0 +1,6 @@
+export class AirlineChef {
+    private name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+}
---
+++ src/Person/employee/FlyAttandent.ts
@@ -0,0 +1,8 @@
+export class Attendant {
+    private name: string;
+    private employeeID: string;
+    constructor(name: string, employeeID: string) {
+        this.name = name;
+        this.employeeID = employeeID;
+    }
+}
