11140f1d10d83bef9f7a50e943ed09dc7dfef749
sreyluchly <sreyluch.ly@student.passerellesnumeriques.org>
2024-04-29 10:31:15 +0700

    create function in ticket #22

--- src/Ticket/Ticket.ts
+++ src/Ticket/Ticket.ts
@@ -10,6 +10,25 @@ export class Ticket {
         this.seatNumber = seatNumber;
     }
 
+    ticketDetails(): void {
+        console.log("Ticket Details:");
+        console.log("ID:", this.id);
+        console.log("Fare Basis:", this.fareBasis);
+        console.log("Meal Type:", this.mealType);
+        console.log("Seat Number:", this.seatNumber);
+    }
+    canRefund(): boolean {
+        return this.fareBasis.toLowerCase() === "flex";
+    }
 
     
 }
+
+
+const ticket1 = new Ticket(1, "Economy Classic", "Vegetarian", "23A");
+ticket1.ticketDetails();
+console.log("Can refund:", ticket1.canRefund() ? "Yes" : "No");
+
+const ticket2 = new Ticket(2, "Flex", "Meal", "23A");
+ticket2.ticketDetails();
+console.log("Can refund:", ticket2.canRefund() ? "Yes" : "No");
