434f5cb42d310ddb8919025b8f0311782f39f376
sreyluchly <sreyluch.ly@student.passerellesnumeriques.org>
2024-05-02 13:30:11 +0700

    imp payment

--- src/Payment/Payment.ts
+++ src/Payment/Payment.ts
@@ -1,33 +1,48 @@
-import { Ticket} from "../Ticket/Ticket";
+import { Ticket } from "../Ticket/Ticket";
 
-export class Payment  {
+export class Payment {
     private paymentId: number;
     private amount: string;
     private ticket: Ticket | null;
 
-    constructor(id: number, amount: string, ticket: Ticket | null = null) {
-        this.paymentId = id;
+    constructor(paymentId: number, amount: string, ticket: Ticket | null = null) {
+        this.paymentId = paymentId;
         this.amount = amount;
         this.ticket = ticket;
     }
 
     calculateTotalAmount(): string {
-        // Example: For simplicity, return the amount as is
+
         return this.amount;
     }
 
-    // Getter method for payment ID
     getPaymentId(): number {
         return this.paymentId;
     }
 
-  
-}
+    getAmount(): string {
+        return this.amount;
+    }
 
+    setAmount(newAmount: string) {
+        if (!newAmount || isNaN(parseFloat(newAmount))) {
+            throw new Error("Invalid amount");
+        }
+        this.amount = newAmount;
+    }
+
+    getTicket(): Ticket | null {
+        return this.ticket;
+    }
+
+    setTicket(newTicket: Ticket | null) {
+        this.ticket = newTicket;
+    }
+}
 
 const ticket1 = new Ticket(1, "Economy Classic", "Vegetarian", "23A");
 const payment = new Payment(1, "50.00$", ticket1);
-console.log(ticket1);
+
+console.log("Ticket:", ticket1);
 console.log("Payment ID:", payment.getPaymentId());
 console.log("Total Amount:", payment.calculateTotalAmount());
-
